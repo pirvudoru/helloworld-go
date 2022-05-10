517cae74001c34478ce6c8d553dd1e04288ca039
pirvudoru <pirvu.doru@ymail.com>
2022-05-10 16:44:57 +0300

    Add language support

--- hello_test.go
+++ hello_test.go
@@ -3,14 +3,24 @@ package main
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	t.Run("saying hello to people	", func(t *testing.T) {
-		got := Hello("Chris")
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 		assertEqual(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertEqual(t, got, want)
+	})
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertEqual(t, got, want)
+	})
+	t.Run("say hello in French", func(t *testing.T) {
+		got := Hello("Claire", "French")
+		want := "Bonjour, Claire"
 		assertEqual(t, got, want)
 	})
 }
