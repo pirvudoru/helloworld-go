e9ea53ce0ef813218d9698b205e097257fa6dafc
pirvudoru <pirvu.doru@ymail.com>
2022-05-10 16:28:43 +0300

    Add hello world

---
+++ hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestSayHello(t *testing.T) {
+	t.Run("saying hello to people	", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertEqual(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertEqual(t, got, want)
+	})
+}
+
+func assertEqual(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
